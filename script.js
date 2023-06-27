aa88d8b755fe219a8350f14fc2da7c145cd84b2d
Diego Sousa <khesanh@Diegos-MacBook-Pro.local>
2023-06-27 11:53:44 -0400

    Created player class + split deck for each player

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//classes 
 class Card {
   constructor(suit,rank,score) {
     this.suit = suit
@@ -15,10 +16,10 @@ class Deck{
 
     createDecks(){
       let suits = ['hearts', 'spades', 'clubs', 'diamonds'];
-      let ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'King', 'Queen'];
+      let ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'King', 'Queen','Ace'];
       for (let i = 0; i < suits.length; i++){
         for (let j = 0; j < ranks.length; j++){
-          let card = new Card(suits[i], ranks[j], j+1);
+          let card = new Card(suits[i], ranks[j], j+2);
           this.cards.push(card);
         }
       }
@@ -38,4 +39,30 @@ class Deck{
       let random = Math.floor(Math.random() * this.cards.length);
       return this.cards[random];
     }
+}
+
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.hand = [];
+    this.score = 0;
+  }
+
+  draw(deck) {
+    this.hand.push(deck.draw());
+  }
+}
+
+//create deck
+let deck = new Deck();
+
+
+//create players
+let player1 = new Player('Player 1');
+let player2 = new Player('Player 2');
+
+//evenly split deck between players
+for (let i = 0; i < 26; i++){
+  player1.draw(deck);
+  player2.draw(deck);
 }
